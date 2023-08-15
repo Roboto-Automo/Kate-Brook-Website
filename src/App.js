37a892dda3e9ae5111fe5dc0b919e6378becf26b
Automoroboto <roberto@studiodespirito.co.uk>
2023-08-15 17:16:32 +0100

    add links to icons and updated burgermenu states

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/home';
-import Book from './pages/book';
-import Other from './pages/other';
-import Navbar from './Components/Navbar';
-import insta from './images/insta.jpg';
-import twitter from './images/twitter.jpg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Book from "./pages/book";
+import Other from "./pages/other";
+import Navbar from "./Components/Navbar";
+import insta from "./images/insta.jpg";
+import twitter from "./images/twitter.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <Router>  
-    <Navbar />
-    <Routes>  
-    <Route exact path='/' element={<Home />} />
-        <Route path='/book' element={<Book/>} />
-        <Route path='/other' element={<Other/>} />
-    </Routes> 
-    <div className="IconsContainer">
-                    <img className="Icon" src={twitter} alt="Icon 2" />
-                    <img className="Icon" src={insta} alt="Icon 1" />
-                </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/other" element={<Other />} />
+      </Routes>
+      <div className="IconsContainer">
+        <a href="https://www.twitter.com/katebrook1">
+          <img className="Icon" src={twitter} alt="Icon 2" />
+        </a>
+        <a href="https://www.instagram.com/kate_br00k/">
+          <img className="Icon" src={insta} alt="Icon 1" />
+        </a>
+      </div>
     </Router>
   );
 }
