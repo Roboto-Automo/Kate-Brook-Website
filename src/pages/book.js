0306c8ecd8844ce951cb1490780998b38f81ac7a
Automoroboto <roberto@studiodespirito.co.uk>
2023-08-18 12:28:45 +0100

    centralised all containers and finished adding quotes

--- src/pages/book.js
+++ src/pages/book.js
@@ -11,7 +11,7 @@ const images = [
    
 
     return (
-        <div className='bookpagecontainter'>
+        <div className='bookpagecontainer'>
         <div className='book'>
         <div className='carousel-container'>
             <Carousel className='crsl' showStatus={false}showThumbs={false} autoPlay={true} infiniteLoop={true} dynamicHeight={true} animationHandler={"fade"} swipeable={false} transitionTime={3000} interval={5000}>
@@ -24,7 +24,7 @@ const images = [
                 })}
             </Carousel>
         </div>
-      
+        
             <section className="synopsis">
             <br></br><br></br>Hazel and Alfie have just moved in together as flatmates. They've also just slept together, which was either a catastrophic mistake, or the best decision of their lives.
 <br></br><br></br>
@@ -35,12 +35,14 @@ What follows will bind the four of them together, bringing joy and heartache, ho
 <div className='outnow'>Out now in hardback (UK) and paperback (US)</div>
 <div className='buttonholder'><button className='buyedition'>Buy UK edition</button><button className='buyedition'>Buy US edition</button></div>
 </section>
-        </div>
 
-        <section className='topcomment'>'I absolutely loved every word of it.'
+        </div>
+        <div className='topcomment'>'I absolutely loved every word of it.'
 <br></br>
-<em>Netgalley review</em></section>
+<em>Netgalley review</em></div>
+
 <section className='quotegrid'>
+<div className='quotebox'>
     <div className='quote'>'I adored this book - fresh, funny and thought provoking, I fell in love with the characters and did not want it to end'
     <br></br>
 <em>Sophie Cousens, author of This Time Next Year</em></div>
@@ -49,10 +51,19 @@ What follows will bind the four of them together, bringing joy and heartache, ho
 <em>Nicola Gill, author of We Are Family</em></div>
     <div className='quote'>'Captures the intricacies of modern relationships with undeniable skill, heaps of humour and a style that fans of Sally Rooney will love. Captivating and addictively complex ... a delight'
 <br></br>
-Ashley Hickson-Lovence, author of Your Show</div>
-    <div className='quote'></div>
-    <div className='quote'></div>
-    <div className='quote'></div>
+<em>Ashley Hickson-Lovence, author of Your Show</em></div>
+</div>
+<div className='quotebox'>
+    <div className='quote'>'A charming peek inside the messy world of modern dating, blending hard-hitting realities with frivolous fun'
+    <br></br>
+<em>Booklist</em></div>
+    <div className='quote'>'Brook's ability to balance humour with explorations of heartbreak, anxiety, and betrayal is admirable....an entertaining tale from start to finish, with characters you'll miss right after finishing the epilogue'
+    <br></br>
+<em>Kirkus</em></div>
+    <div className='quote'>'Brook's enjoyable debut tackles the messiness of love and family... Heartfelt and entertaining'
+    <br></br>
+<em>Publisher's Weekly</em></div>
+    </div>
 </section>
         </div>
     )
