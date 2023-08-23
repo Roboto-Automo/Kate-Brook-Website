5fdf090d69f59699e9094c84ebac518dbaa2c705
Automoroboto <roberto@studiodespirito.co.uk>
2023-08-23 16:33:13 +0100

    add hover states on navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,44 @@
+import React, { useState } from 'react';
 import './Navbar.css';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import BurgerMenu from './burgermenu';
 
 export default function Navbar() {
+    const [activeLink, setActiveLink] = useState("about");
+
+    const handleLinkClick = (link) => {
+        setActiveLink(link);
+    };
+
     return (
         <div className="navbar">
-        {/* Show original links on larger screens */}
-        <div className="original-links">
-            <Link to="/">About</Link>
-            <Link to="/book">Not Exactly What I Had in Mind</Link>
-            <Link to="/other">Other Writing</Link>
+            {/* Show original links on larger screens */}
+            <div className="original-links">
+                <Link
+                    to="/"
+                    onClick={() => handleLinkClick('about')}
+                    className={`navbar-link ${activeLink === 'about' ? 'active-link' : ''}`}
+                >
+                    About
+                </Link>
+                <Link
+                    to="/book"
+                    onClick={() => handleLinkClick('book')}
+                    className={`navbar-link ${activeLink === 'book' ? 'active-link' : ''}`}
+                >
+                    Not Exactly What I Had in Mind
+                </Link>
+                <Link
+                    to="/other"
+                    onClick={() => handleLinkClick('other')}
+                    className={`navbar-link ${activeLink === 'other' ? 'active-link' : ''}`}
+                >
+                    Other Writing
+                </Link>
+            </div>
+
+            {/* Show burger menu on smaller screens */}
+            <BurgerMenu />
         </div>
-        
-        {/* Show burger menu on smaller screens */}
-        <BurgerMenu />
-    </div>
-    )
+    );
 }
